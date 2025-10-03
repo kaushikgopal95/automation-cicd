14e62f770d6949bc4b20f91aae7c804907ef0ac5
kaushikgopal95 <kaushik.gopal95@gmail.com>
2025-10-03 14:47:43 +0530

    TC-21 to Tc-30

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -10,14 +10,14 @@ describe('PlantBot App Tests', () => {
 it('TC-21: Validate to check if validation error message is displayed when user sign up field is empty', () => {
 cy.get('[data-testid="get-started-btn"]').click()
 cy.get('button[type="submit"]').contains('Create Account').click()
-cy.get('input[name="fullName"]').should('have.attr', 'required');
+cy.get('p').should('contain', 'Full name is required');
 })
 
 it('TC-22: Validate to check if user can enter valid details on Full name field during sign up', () => {
 cy.get('[data-testid="get-started-btn"]').click()
 cy.get('input[name="fullName"]').type('John Paul')
 cy.get('button[type="submit"]').contains('Create Account').click()
-cy.get('input[name="phone"]').should('have.attr', 'required');
+cy.get('p').should('contain', 'Phone number is required');
 })
 
 it('TC-23: Validate to check if user can select "User Type" from the dropdown during sign up', () => {
@@ -32,7 +32,7 @@ cy.get('[data-testid="get-started-btn"]').click()
 cy.get('input[name="fullName"]').type('John Paul')
 cy.get('select').select('Business')
 cy.get('button[type="submit"]').contains('Create Account').click()
-cy.get('input[name="phone"]').should('have.attr', 'required');
+cy.get('p').should('contain', 'Phone number is required');
 })
 
 
@@ -42,11 +42,7 @@ cy.get('input[name="fullName"]').type('John Paul')
 cy.get('select').select('Business')
 cy.get('input[name="phone"]').type('12345')
 cy.get('button[type="submit"]').contains('Create Account').click()
-cy.get('input[name="phone"]').then(($input) => {
-    const inputElem = $input[0] as HTMLInputElement;
-    expect(inputElem.checkValidity()).to.be.false;
-    expect(inputElem.validationMessage).to.include('Please match');
-  });
+cy.get('p').should('contain', 'Phone number must be exactly 10 digits');
 })
 
 it('TC-26: Validate to check if user can select a country from the dropdown during sign up', () => {
@@ -68,7 +64,7 @@ cy.get('input[name="phone"]').type('1234567890')
 cy.get('button[role="combobox"]').click();
 cy.get('input[placeholder="Search countries..."]').should('be.visible').type('Canada{enter}');
 cy.get('button[type="submit"]').contains('Create Account').click()
-cy.get('input[name="email"]').should('have.attr', 'required');
+cy.get('p').should('contain', 'Email is required');
 })
 
 it('TC-28: Validate to check if user can enter invalid email during sign up', () => {
@@ -80,5 +76,35 @@ cy.get('button[role="combobox"]').click();
 cy.get('input[placeholder="Search countries..."]').should('be.visible').type('Canada{enter}');
 cy.get('input[name="email"]').type('invalidemail')
 cy.get('button[type="submit"]').contains('Create Account').click()
+cy.get('p').should('contain', 'Please enter a valid email address');
 })
+
+
+it('TC-29: Validate to check if user can enter valid email during sign up', () => {
+cy.get('[data-testid="get-started-btn"]').click()
+cy.get('input[name="fullName"]').type('John Paul')
+cy.get('select').select('Business')
+cy.get('input[name="phone"]').type('1234567890')
+cy.get('button[role="combobox"]').click();
+cy.get('input[placeholder="Search countries..."]').should('be.visible').type('Canada{enter}');
+cy.get('input[name="email"]').type('kaushik.test@gmail.com')
+cy.get('button[type="submit"]').contains('Create Account').click()
+cy.get('p').should('contain', 'Password is required');
+})
+
+it('TC-30: Validate to check if user can enter invalid password combination during sign up', () => {
+cy.get('[data-testid="get-started-btn"]').click()
+cy.get('input[name="fullName"]').type('John Paul')
+cy.get('select').select('Business')
+cy.get('input[name="phone"]').type('1234567890')
+cy.get('button[role="combobox"]').click();
+cy.get('input[placeholder="Search countries..."]').should('be.visible').type('Canada{enter}');
+cy.get('input[name="email"]').type('kaushik.test@gmail.com')
+cy.get('input[name="password"]').type('short')
+cy.get('span').should('contain', 'At least 8 characters')
+cy.get('span').should('contain', 'Weak')
+})
+
+
+
 })
